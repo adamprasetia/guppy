708280b4dc748466231aaf8e178443881e60e69c
Adam Prasetia <adam.prasetia@kompas.com>
2022-12-08 08:03:02 +0700

    validation

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -14,5 +14,15 @@ class MY_Controller extends CI_Controller
             $this->session_login = $this->session->userdata('session_login');
             $this->session_store = $this->session->userdata('session_store');
         }
+
+        // validation owner
+        if(in_array($this->uri->segment(1),['item','sell','trans','buy']) && $this->uri->segment(2)=='edit'){
+            $table = $this->uri->segment(1);
+            $id = $this->uri->segment(3);
+            $data = $this->db->where('id', $id)->get($table)->row();
+            if($data->store_id != $this->session_store){
+                redirect($table);
+            }
+        }
     }
 }
