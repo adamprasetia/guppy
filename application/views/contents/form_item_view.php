237abefc93bdf9caa12574d28f94e0dfc89f33ef
Adam Prasetia <adam.prasetia@kompas.com>
2022-12-13 08:19:19 +0700

    fix

--- application/views/contents/form_item_view.php
+++ application/views/contents/form_item_view.php
@@ -14,12 +14,13 @@
             <table class="table table-bordered">
             <input type="hidden" id="id" name="id" value="<?php echo isset($data->id)?$data->id:'' ?>">
             <tr>
-                <td><label>Nama Produk *</label></td>
-                <td><input type="text" id="name" name="name" class="form-control" value="<?php echo isset($data->name)?htmlentities($data->name):'' ?>"></td>
-            </tr>
-            <tr>
                 <td><label>Kode Produk *</label></td>
                 <td><input type="text" id="sku" name="sku" class="form-control" value="<?php echo isset($data->sku)?htmlentities($data->sku):'' ?>"></td>
+                <script>document.getElementById("sku").focus()</script>
+            </tr>
+            <tr>
+                <td><label>Nama Produk *</label></td>
+                <td><input type="text" id="name" name="name" class="form-control" value="<?php echo isset($data->name)?htmlentities($data->name):'' ?>"></td>
             </tr>
             <tr>
                 <td><label>Harga Beli</label></td>
@@ -33,6 +34,7 @@
         </div>
         <div class="box-footer">
             <button type="button" class="btn_action btn btn-primary" data-redirect="<?php echo base_url('item/index').get_query_string() ?>" data-action="<?php echo $action ?>" data-form="#form_data" data-idle="<i class='fa fa-save'></i> Simpan" data-process="Menyimpan..."><i class='fa fa-save'></i> Simpan</button>
+            <button type="button" class="btn_action btn btn-primary" data-redirect="<?php echo base_url('item/add').get_query_string() ?>" data-action="<?php echo $action ?>" data-form="#form_data" data-idle="<i class='fa fa-save'></i> Simpan Lalu Tambah Lagi" data-process="Menyimpan..."><i class='fa fa-save'></i> Simpan Lalu Tambah Lagi</button>
             <button type="button" class="btn_close btn btn-default" data-redirect="<?php echo base_url('item/index').get_query_string() ?>"><i class='fa fa-close'></i> Kembali</button>
         </div>
     </form>
